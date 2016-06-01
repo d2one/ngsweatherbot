4a00906ce1a8b5010806e61f898a1f7f13ac6a8a
Kuznetsov Daniil <kuznetsov.daniil.d2@gmail.com>
2016-06-01 22:15:07 +0600

    Base bot implementation

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"gopkg.in/telegram-bot-api.v4"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"github.com/bot-api/telegram"
+	"github.com/bot-api/telegram/telebot"
+	"golang.org/x/net/context"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,51 +14,125 @@ import (
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("TOKEN")
-	if err != nil {
-		log.Panic(err)
-	}
+	api := telegram.New("TOKEN")
+	api.Debug(true)
+	bot := telebot.NewWithAPI(api)
+	bot.Use(telebot.Recover()) // recover if handler panic
+	boltStorage := NewBoltStorage("bolt.db")
+	netCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
+	bot.HandleFunc(func(ctx context.Context) error {
+		update := telebot.GetUpdate(ctx) // take update from context
 		if update.Message == nil {
-			continue
+			return nil
 		}
 
-		resp, err := http.Get("http://pogoda.ngs.ru/api/v1/forecasts/current?city=novosibirsk")
-		if err != nil {
-			// handle error
-		}
+		textMessage := "No selected city. Select city with command /city {cityName}"
 
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err.Error())
-		}
+		boltStorage.DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("users"))
+			v := b.Get([]byte(strconv.FormatInt(update.From().ID, 10)))
+			if v != nil {
+				fmt.Printf("The answer is: %s\n", v)
+				textMessage = getStations(string(v))
+			}
+			return nil
+		})
 
-		messageText := getStations([]byte(body))
+		api := telebot.GetAPI(ctx) // take api from context
+		msg := telegram.NewMessage(update.Chat().ID, textMessage)
+		_, err := api.Send(ctx, msg)
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, messageText)
-		bot.Send(msg)
+		return err
 
-		// icon := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, "img/cloudy_none_day_big.png")
-		// icon.Caption = messageText
-		// bot.Send(icon)
+	})
 
+	// Use command middleware, that helps to work with commands
+	bot.Use(telebot.Commands(map[string]telebot.Commander{
+		"start": telebot.CommandFunc(
+			func(ctx context.Context, arg string) error {
+
+				api := telebot.GetAPI(ctx)
+				update := telebot.GetUpdate(ctx)
+				_, err := api.SendMessage(ctx,
+					telegram.NewMessagef(update.Chat().ID,
+						"received start with arg %s", arg,
+					))
+				return err
+			}),
+		"city": telebot.CommandFunc(
+			func(ctx context.Context, arg string) error {
+
+				api := telebot.GetAPI(ctx)
+				update := telebot.GetUpdate(ctx)
+				user := update.From()
+				log.Printf("UUUUUUUUUUUUSER %i %s %s", user.ID, user.FirstName, user.LastName)
+
+				city, error := getCities(arg)
+
+				if error != "" {
+					_, err := api.SendMessage(ctx,
+						telegram.NewMessagef(update.Chat().ID, error))
+					return err
+				}
+
+				boltStorage.writerChan <- [3]interface{}{"users", strconv.FormatInt(user.ID, 10), []byte(city)}
+
+				_, err := api.SendMessage(ctx,
+					telegram.NewMessagef(update.Chat().ID,
+						"received test with arg %s", city,
+					))
+				return err
+			}),
+	}))
+
+	err := bot.Serve(netCtx)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-func getStations(body []byte) string {
+func getCities(arg string) (string, string) {
+	resp, err := http.Get("http://pogoda.ngs.ru/api/v1/cities?q=" + arg)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var s = new(WeatherCitys)
+	err = json.Unmarshal(body, &s)
+	if err != nil {
+		log.Printf("whoops:", err)
+	}
+
+	if s.Errors.Message != "" {
+		return "", s.Errors.Message
+	}
+
+	return s.Cities[0].Alias, ""
+
+}
+
+func getStations(arg string) string {
+	resp, err := http.Get("http://pogoda.ngs.ru/api/v1/forecasts/current?city=" + arg)
+	if err != nil {
+		// handle error
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	var s = new(WeatherResponce)
-	err := json.Unmarshal(body, &s)
+	err = json.Unmarshal(body, &s)
 	if err != nil {
 		log.Printf("whoops:", err)
 	}
@@ -62,6 +140,72 @@ func getStations(body []byte) string {
 	messageText := strconv.FormatFloat(float64(s.Forecasts[0].Temperature), 'f', 1, 32) + "°C, " + "Ветер: " + strconv.FormatFloat(float64(s.Forecasts[0].Wind.Speed), 'f', 1, 32) + "м/с, " + s.Forecasts[0].Wind.Direction.Title + ", " + s.Forecasts[0].Cloud.Title + " " + s.Forecasts[0].Precipitation.Title
 
 	return messageText
+}
+
+func (this *BoltStorage) writer() {
+	for data := range this.writerChan {
+		bucket := data[0].(string)
+		keyId := data[1].(string)
+		dataBytes := data[2].([]byte)
+		err := this.DB.Update(func(tx *bolt.Tx) error {
+			sesionBucket, err := tx.CreateBucketIfNotExists([]byte(bucket))
+			if err != nil {
+				return err
+			}
+			sesionBucket.Delete([]byte(keyId))
+			return sesionBucket.Put([]byte(keyId), dataBytes)
+		})
+		if err != nil {
+			// TODO: Handle instead of panic
+			panic(err)
+		}
+	}
+}
+
+func NewBoltStorage(dbPath string) *BoltStorage {
+	db, err := bolt.Open(dbPath, 0666, nil)
+	writerChan := make(chan [3]interface{})
+	boltStorage := &BoltStorage{DB: db, writerChan: writerChan}
+	boltStorage.DB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("users"))
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	go boltStorage.writer()
+	if err != nil {
+		panic(err)
+	}
+	return boltStorage
+}
+
+type WeatherCitys struct {
+	Cities []struct {
+		Alias              string      `json:"alias"`
+		ID                 int         `json:"id"`
+		MobileURL          string      `json:"mobile_url"`
+		Name               string      `json:"name"`
+		ProjectName        string      `json:"project_name"`
+		Region             int         `json:"region"`
+		Timezone           string      `json:"timezone"`
+		Title              string      `json:"title"`
+		TitleDative        string      `json:"title_dative"`
+		TitleForIos        interface{} `json:"title_for_ios"`
+		TitlePrepositional string      `json:"title_prepositional"`
+		URL                string      `json:"url"`
+	} `json:"cities"`
+	Errors struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	} `json:"errors"`
+	Metadata struct {
+		Resultset struct {
+			Count  int `json:"count"`
+			Limit  int `json:"limit"`
+			Offset int `json:"offset"`
+		} `json:"resultset"`
+	} `json:"metadata"`
 }
 
 type WeatherResponce struct {
@@ -125,4 +269,9 @@ type WeatherResponce struct {
 			Count int `json:"count"`
 		} `json:"resultset"`
 	} `json:"metadata"`
+}
+
+type BoltStorage struct {
+	DB         *bolt.DB
+	writerChan chan [3]interface{} //not so agnostic but enough now
 }
